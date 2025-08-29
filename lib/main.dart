2bb1837aaccea9549314319705c51ca12708a4a1
Harsh <coolhkay1@gmail.com>
2025-08-29 10:44:55 +0530

    added new line support

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,5 @@ void main() {
   print(add(''));
   print(add('12'));
   print(add('1,2,3'));
+  print(add('1\n2,5'));
 }
--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,8 +1,12 @@
-int add(String number) {
-  if (number.isEmpty) {
+int add(String numbers) {
+  if (numbers.isEmpty) {
     return 0;
   }
-  final List<String> numbers = number.split(',');
 
-  return numbers.fold(0, (a, b) => a + int.parse(b));
+  final String delimiter = ',';
+
+  numbers = numbers.replaceAll('\n', delimiter);
+  final List<String> number = numbers.split(delimiter);
+
+  return number.fold(0, (a, b) => a + int.parse(b));
 }
